122c7e7bd6b75c5f75987c4cf45a1e14ea49c1a7
yasuo- <breitling1216@gmail.com>
2018-02-14 16:43:22 +0900

    Add react-navigation

--- src/native/App.js
+++ src/native/App.js
@@ -5,7 +5,9 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { TabNavigator, TabBarBottom } from "react-navigation";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -15,13 +17,17 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+class HomeScreen extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button
+          title="Go to Settings"
+          onPress={() => this.props.navigation.navigate("Settings")}
+        />
       </View>
     );
   }
@@ -45,3 +51,75 @@ const styles = StyleSheet.create({
     marginBottom: 5
   }
 });
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate("Home")}
+        />
+      </View>
+    );
+  }
+}
+
+/*
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen
+    },
+    Details: {
+      screen: DetailsScreen
+    }
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+
+export default class App extends Component {
+  render() {
+    return <RootStack />;
+  }
+}
+*/
+
+export default TabNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Settings: { screen: DetailsScreen }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = `ios-information-circle${focused ? "" : "-outline"}`;
+        } else if (routeName === "Settings") {
+          iconName = `ios-options${focused ? "" : "-outline"}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        // return <Ionicons name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray",
+      style: {
+        backgroundColor: "white"
+      }
+    },
+    animationEnabled: false,
+    swipeEnabled: false
+  }
+);
