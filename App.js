9e72d6dbe7f743c6325a034f017f4b32a754fd57
yasuo- <breitling1216@gmail.com>
2018-03-01 18:57:09 +0900

    Add: Expoに合わせるように対応

---
+++ App.js
@@ -0,0 +1,13 @@
+/**
+ * React Native App EXPO用
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React from "react";
+import AppScreen from "./src/native/App";
+
+export default class App extends React.Component {
+  render() {
+    return <AppScreen />;
+  }
+}
