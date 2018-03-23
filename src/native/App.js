4b4000a309284a9cc39a2017b0a1e737cd116af8
yasuo- <breitling1216@gmail.com>
2018-03-23 20:03:56 +0900

    Update: Expo対応

--- src/native/App.js
+++ src/native/App.js
@@ -1,125 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+/* @flow */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Button } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { TabNavigator, TabBarBottom } from "react-navigation";
+import { View } from "react-native";
+import { StackNavigator, TabNavigator, TabBarBottom } from "react-navigation";
+import { Font, AppLoading } from "expo";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import Fonts from "./config/fonts/index";
+import { withRkTheme } from 'react-native-ui-kitten';
+import RootStack from "./navigator";
 
-type Props = {};
-class HomeScreen extends Component<Props> {
+interface AppState {
+  ready: boolean;
+}
+
+const cacheFonts = (fonts) => (fonts.map(font => Font.loadAsync(font)))
+
+export default class App extends Component<{}, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = { ready: false };
+  }
+
+  componentDidMount() {
+    this.loadStaticResources();
+  }
+
+  async loadStaticResources(): Promise<void> {
+    const fontAssets = cacheFonts(Fonts);
+    await Promise.all([...fontAssets]);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Button
-          title="Go to Settings"
-          onPress={() => this.props.navigation.navigate("Settings")}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate("Home")}
-        />
-      </View>
-    );
-  }
-}
-
-/*
-const RootStack = StackNavigator(
-  {
-    Home: {
-      screen: HomeScreen
-    },
-    Details: {
-      screen: DetailsScreen
-    }
-  },
-  {
-    initialRouteName: "Home"
-  }
-);
-
-
-export default class App extends Component {
-  render() {
-    return <RootStack />;
-  }
-}
-*/
-
-export default TabNavigator(
-  {
-    Home: { screen: HomeScreen },
-    Settings: { screen: DetailsScreen }
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === "Home") {
-          iconName = `ios-information-circle${focused ? "" : "-outline"}`;
-        } else if (routeName === "Settings") {
-          iconName = `ios-options${focused ? "" : "-outline"}`;
+      <View style={{ flex: 1 }}>
+        {this.state.ready
+          ? <RootStack />
+          : <AppLoading
+            startAsync={this._loadAssetsAsync}
+            onFinish={() => this.setState({ ready: true })}
+            onError={console.warn}
+          />
         }
-
-        // You can return any component that you like here! We usually use an
-        // icon component from react-native-vector-icons
-        // return <Ionicons name={iconName} size={25} color={tintColor} />;
-      }
-    }),
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: "bottom",
-    tabBarOptions: {
-      activeTintColor: "tomato",
-      inactiveTintColor: "gray",
-      style: {
-        backgroundColor: "white"
-      }
-    },
-    animationEnabled: false,
-    swipeEnabled: false
+      </View>
+    );
   }
-);
+}
